5002d814cda04e99218764b0c22a158e1e3246ca
Fernando Konflanz <konflanz.info@gmail.com>
2021-04-20 15:21:33 -0300

    Update SHA1.java
    Atualizando.

--- SHA1.java
+++ SHA1.java
@@ -1,55 +1,61 @@
+import javax.xml.bind.DatatypeConverter;
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.util.Arrays;
 import java.util.Base64;
 
 public class SHA1 {
 
-	// Esta função gera uma base64 de uma criptografia SHA1.
-	public static String gerarHashEntrega(String chCTe, String base64ImagemEntrega) {
+    // Esta função gera uma base64 de uma criptografia SHA1.
+    public static String gerarHashEntrega(String chCTe, String base64ImagemEntrega) {
 
-		String sha1 = "";
-		String hash = chCTe + base64ImagemEntrega;
-		String hashEntrega = "";
+        String sha1 = "";
+        String hash = chCTe + base64ImagemEntrega;
+        String hashEntrega = "";
 
-		try {
+        try {
 
-			// Gera uma criptografia SHA1 = 20 bytes, 40 caracteres
-			MessageDigest digest = MessageDigest.getInstance("SHA-1"); 
-			digest.reset();
-			digest.update(hash.getBytes("utf8"));
+            // Gera uma criptografia SHA1 = 20 bytes, 40 caracteres
+            MessageDigest digest = MessageDigest.getInstance("sha-1");
+            digest.reset();
+            digest.update(hash.getBytes(StandardCharsets.UTF_8));
 
-			// Gera uma string Hexadecimal da criptografia SHA1
-			sha1 = String.format("%040x", new BigInteger(1, digest.digest())); 
+            // Gera uma string Hexadecimal da criptografia SHA1
+            sha1 = String.format("%040x", new BigInteger(1, digest.digest()));
 
-			// Converte a string hexadecimal para um array de bytes
-			byte[] b = new BigInteger(sha1, 16).toByteArray();
+            // Converte a string hexadecimal para um array de bytes
+            byte[] b = DatatypeConverter.parseHexBinary(sha1);
 
-			// Converte o Array de Bytes para uma base64
-			hashEntrega = Base64.getEncoder().encodeToString(b);
-		}
+            // Converte o Array de Bytes para uma base64
+            hashEntrega = Base64.getEncoder().encodeToString(b);
 
-		catch (Exception e) {
-			e.printStackTrace();
-		}
 
-		// Retorna a base64 da SHA1
-		return hashEntrega;
-	}
+        }
 
-	// Exemplo de uso da função
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 
-	public static void main(String[] argv) {
+        // Retorna a base64 da SHA1
+        return hashEntrega;
+    }
 
-		String chCTe = ""; // Chave do CT-e
+    // Exemplo de uso da função
+    public static void main(String[] argv) {
+	    
+	    // Chave do CT-e
+        String chCTe = ""; 
+	
+	    // Base64 da imagem capturada da entrega (Exemplo: imagem capturada da assinatura eletrônica, digital do recebedor, foto, etc).    
+        String base64ImagemEntrega = ""; 
 
-		String base64ImagemEntrega = ""; // Base64 da imagem capturada da entrega (Exemplo: imagem capturada da assinatura eletrônica, digital do recebedor, foto, etc).
+        // Chamada da função.
+        String hashGerada = gerarHashEntrega(chCTe, base64ImagemEntrega);
 
-		// Chamada da função.
-		String hashGerada = gerarHashEntrega(chCTe, base64ImagemEntrega);
+        // Para visualizar a hash para ser informada no campo do evento do CTe
+        System.out.println("hashEntrega : " + hashGerada);
 
-		// Para visualizar a hash para ser informada no campo do evento do CTe
-		System.out.println("hash gerada: " + hashGerada); 
-
-	}
+    }
 
 }
